cf025e1395a623a43deb4eb4f2dec6194a1bcdb0
Adam Dudley <dudleyadam@gmail.com>
2018-11-29 09:28:57 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(items)
+  items.sort
+end
 
+def sort_array_desc(items)
+  items.sort.reverse
+end
+
+def sort_array_char_count(items)
+  items.sort_by { |item| item.length }
+end
+
+def swap_elements(items)
+  item1 = items[1]
+  item2 = items[2]
+  items[1] = item2
+  items[2] = item1
+  items
+end
+
+def reverse_array(items)
+  items.reverse
+end
+
+def kesha_maker(items)
+  items.each { |string| string[2] = '$' }
+end
+
+def find_a(items)
+  #items.find #{ |string| string[0] = 'a' } #.start_with?
+  items.select { |string| string.start_with?('a') }
+end
+
+def sum_array(items)
+  #items.each do |item| item + item
+  items.inject(:+)
+end
+
+def add_s(items)
+  items.each_with_index.map do |string, index|
+    if index == 1
+      string
+    else
+      string + "s"
+    end
+  end
+end
